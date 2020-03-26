e8e46c000baa2b3e0ef5559124398cb289cf8fe4
GianmarcoMontanari <giammont@live.it>
2020-03-26 18:18:04 +0100

    aggiunta css

--- main.js
+++ main.js
@@ -5,16 +5,32 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz”
 
 // listare i numeri da 1 a 100
+
+var cifre = "";;
+
+
 for(var numero = 1; numero <= 100; numero++) {
-  // Se è multiplo di 3 allora stampo FizzBuzz
+  // Se è divisibile per 15 FizzBuzz
   if(numero % 15 == 0) {
       console.log("FizzBuzz");
+      cifre = cifre + "<div>FizzBuzz</div>";
+
+      // se è multiplo di 3 scrivo Fizz
   } else if (numero % 3 == 0){
       console.log("Fizz");
+      cifre = cifre + " <div> Fizz </div>";
+
+      // se è multiplo di 5 scrivo buzz
   } else if(numero % 5 == 0) {
       console.log("Buzz");
+      cifre = cifre + "<div>  Buzz </div> ";
+
+      // in tutti gli altri casi scrivo il numero
   } else {
       console.log(numero);
+      cifre = cifre + "<div>" + numero + "</div>" ;
   }
 
 }
+
+document.getElementById("stampa").innerHTML = cifre;
